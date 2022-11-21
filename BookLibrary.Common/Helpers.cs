895184a58b110e741dc2c812090a9830ae2da745
Håkan Sjö Ballina <hakan@badpixel.se>
2022-11-21 21:02:38 +0100

    Add Helper to Common

---
+++ BookLibrary.Common/Helpers.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+namespace BookLibrary.Common;
+
+public static class Helpers
+{
+  public static IMapper GetMapper(IEnumerable<Profile> profiles)
+  {
+    var config = new MapperConfiguration(cfg =>
+    {
+      cfg.AddProfiles(profiles);
+    });
+
+    return new Mapper(config);
+  }
+}
