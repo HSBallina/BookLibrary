e0e177145425db6f7f4c8e2afbc207e329a9d95f
Håkan Sjö Ballina <hakan@badpixel.se>
2022-11-21 12:47:24 +0100

    Add Common project

---
+++ BookLibrary.Common/Dtos/AuthorDto.cs
@@ -0,0 +1,18 @@
+﻿namespace BookLibrary.Common.Dtos
+{
+  public class AuthorDto
+  {
+    public AuthorDto(Guid id, string name)
+    {
+      Id = id;
+      Name = name;
+    }
+
+    public Guid Id { get; }
+    public string Name { get; }
+    public override int GetHashCode() => (Id, Name).GetHashCode();
+    public override bool Equals(object? other)
+      => other is AuthorDto e && (e.Id, e.Name).Equals((Id, Name));
+    public override string ToString() => $"{Id}, {Name}";
+  }
+}
