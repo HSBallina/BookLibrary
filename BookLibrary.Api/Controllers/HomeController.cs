6eabbc06c8fd3863dbdd1916281c7d4511f5cf00
Håkan Sjö Ballina <hakan@badpixel.se>
2022-11-21 11:55:54 +0100

    Tidy up project folders

---
+++ BookLibrary.Api/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookLibrary.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+      [HttpGet]
+      public IActionResult Hello()
+      {
+        return Ok("Hello World");
+      }
+    }
+}
