61da16776f0ac39245bb6a8a3be84071d8daae8c
Håkan Sjö Ballina <hakan@badpixel.se>
2022-11-21 16:13:18 +0100

    Add map profile for authors

---
+++ BookLibrary.Data/Profiles/AuthorProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BookLibrary.Common.Dtos;
+using BookLibrary.Data.Models;
+
+namespace BookLibrary.Data.Profiles;
+
+public class AuthorProfile : Profile
+{
+  public AuthorProfile()
+  {
+    CreateMap<Author, AuthorDto>();
+
+    CreateMap<AuthorDto, Author>()
+      .ForMember(d => d.Books, o => o.Ignore());
+  }
+}
