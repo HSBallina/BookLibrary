cfb6ccefc4e257799b4c82cdccf71bf2a0ee16eb
Håkan Sjö Ballina <hakan@badpixel.se>
2022-11-21 22:55:02 +0100

    Add Book and AuthorDetail DTOs

---
+++ BookLibrary.Common/Dtos/AuthorDetailDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BookLibrary.Common.Dtos;
+
+public class AuthorDetailDto : AuthorDto
+{
+  public AuthorDetailDto(Guid id, string name) : base(id, name)
+  {
+    Books = new List<BookDto>();
+  }
+
+  public IEnumerable<BookDto> Books { get; set; }
+}
---
+++ BookLibrary.Common/Dtos/BookDto.cs
@@ -0,0 +1,17 @@
+﻿namespace BookLibrary.Common.Dtos;
+
+public class BookDto
+{
+  public BookDto(Guid id, string name)
+  {
+    Id = id;
+    Name = name;
+  }
+
+  public Guid Id { get; }
+  public string Name { get;  }
+  public override int GetHashCode() => (Id, Name).GetHashCode();
+  public override bool Equals(object? other)
+    => other is BookDto e && (e.Id, e.Name).Equals((Id, Name));
+  public override string ToString() => $"{Id}, {Name}";
+}
